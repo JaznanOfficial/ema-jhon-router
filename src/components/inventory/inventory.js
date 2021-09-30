0cb978208c04af4920dd9f7a3471f37d1130a14f
JaznanOfficial <jaznanofficial@gmail.com>
2021-09-30 21:00:54 +0600

    inventory added

---
+++ src/components/inventory/inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const inventory = () => {
+    return (
+        <div>
+            <h3>This is inventory</h3>
+        </div>
+    );
+};
+
+export default inventory;
